d0000c8b9bbedfefa23bd2fd298bac6d10d3adf6
Vadym-Gr <aivvfg@gmail.com>
2026-03-01 01:59:19 +0200

    v-2

--- src/componeents/App/App.tsx
+++ src/componeents/App/App.tsx
@@ -1,17 +1,44 @@
+import { useState } from 'react';
 import css from "./App.module.css";
 
 import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from '../VoteOptions/VoteOptions';
 
-function App() {
-  
+import type { Votes, VoteType } from '../../types/votes';
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes(prev => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+   
   return (
     <>
       <div className={css.app}>
         <CafeInfo />
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
+          
       </div>
       
     </>
   )
 }
-
-export default App
