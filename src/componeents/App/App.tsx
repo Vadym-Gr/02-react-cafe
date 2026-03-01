64b45b35dacb9c09434091671663b58b42057749
Vadym-Gr <aivvfg@gmail.com>
2026-03-01 13:51:36 +0200

    v-3

--- src/componeents/App/App.tsx
+++ src/componeents/App/App.tsx
@@ -5,6 +5,8 @@ import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from '../VoteOptions/VoteOptions';
 
 import type { Votes, VoteType } from '../../types/votes';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notif';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
@@ -30,15 +32,30 @@ export default function App() {
 
   const totalVotes = votes.good + votes.neutral + votes.bad;
 
+  const positiveRate = totalVotes > 0 ? Math.round((votes.good / totalVotes) * 100) : 0;
    
   return (
     <>
       <div className={css.app}>
         <CafeInfo />
-        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
-          
-      </div>
-      
+        
+        <VoteOptions
+          onVote={handleVote}
+          onReset={resetVotes}
+          canReset={totalVotes > 0}
+        />
+        
+        {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+        
+      </div>      
     </>
   )
 }
